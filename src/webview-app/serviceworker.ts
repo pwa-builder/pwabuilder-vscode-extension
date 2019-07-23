b6986af9b4e62c7d41774b0f10dc49f6eb309e0b
Justin Willis <jgw9617@gmail.com>
2019-07-23 12:54:06 -0700

    chore(): cleanup + confirm we can get data

--- src/webview-app/serviceworker.ts
+++ src/webview-app/serviceworker.ts
@@ -6,7 +6,7 @@ const vscode = acquireVsCodeApi();
 
 @customElement('simple-greeting')
 export class SimpleGreeting extends LitElement {
-  @property() name = 'World';
+  @property() serviceWorkers = [];
 
   firstUpdated() {
     window.onload = () => {
@@ -14,9 +14,17 @@ export class SimpleGreeting extends LitElement {
         name: 'Ready!!!!!!'
       });
     };
+
+    window.addEventListener('message', (event) => {
+      this.serviceWorkers = event.data.data;
+      const serviceworker$ = this.serviceWorkers[0].id;
+      console.log(serviceworker$);
+    });
   }
 
   render() {
-    return html`<p>Hello, ${this.name}!</p>`;
+    return html`
+      ${this.serviceWorkers.map((i) => html`<div class="serviceWorkerType"><input type="radio"  name="swgroup" value="${i.id}" id="selectsw${i.id}"><div class="swTypeInnerDiv"><h4>${i.title}</h4><span> ${i.description}</span></div></div></br> `)}
+    `;
   }
 }
