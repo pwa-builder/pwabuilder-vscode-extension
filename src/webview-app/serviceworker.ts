136bfe532a507b6b5921f2eca21d19309c2542db
Amrutha Srinivasan <t-amsrin@microsoft.com>
2019-07-24 14:58:47 -0700

    Syntax highlighting and more cases for writing into index file

--- src/webview-app/serviceworker.ts
+++ src/webview-app/serviceworker.ts
@@ -1,7 +1,12 @@
 import { css, LitElement, html, property, customElement } from 'lit-element';
+import { unsafeHTML } from 'lit-html/directives/unsafe-html';
 
+import Prism from 'prismjs';
+
+
+// var Prism = require('prismjs');
 declare var acquireVsCodeApi: any;
-
+// declare var htmlString: any;
 const vscode = acquireVsCodeApi();
 
 @customElement('sw-picker')
@@ -10,15 +15,19 @@ export class SWPicker extends LitElement {
   @property() currentServiceWorker = null;
   @property() webSiteData = 'Loading code...';
   @property() serviceWorkerCode = 'Loading code...';
+  @property() highlightedWebSiteData = '';
+  @property() highlightedSWCode = '';
 
   static get styles() {
     return css`
+    @import url("http://prismjs.com/themes/prism-tomorrow.css");
+    
       main {
         display: flex;
       }
 
       .column {
-        float: left;
+        float: left;    
         width: 40%;
         padding: 20px;
         flex: 1;
@@ -55,9 +64,11 @@ export class SWPicker extends LitElement {
     window.addEventListener('message', async (event) => {
       this.serviceWorkers = event.data.data;
       this.currentServiceWorker = this.serviceWorkers[0].id;
+
       console.log(this.currentServiceWorker);
 
       await this.getCode();
+
     });
   }
 
@@ -67,6 +78,8 @@ export class SWPicker extends LitElement {
 
     this.webSiteData = data.webSite;
     this.serviceWorkerCode = data.serviceWorker;
+    this.highlightedWebSiteData = Prism.highlight(this.webSiteData, Prism.languages.javascript, 'javascript');
+    this.highlightedSWCode = Prism.highlight(this.serviceWorkerCode, Prism.languages.javascript, 'javascript');
   }
 
   async updateChoice(id) {
@@ -76,7 +89,6 @@ export class SWPicker extends LitElement {
   }
 
   download() {
-    console.log('hello world');
     vscode.postMessage({
       name: 'download',
       serviceWorkerId: this.currentServiceWorker,
@@ -99,38 +111,44 @@ export class SWPicker extends LitElement {
     <main>
       <div class="column">
         <h2>Choose a Service Worker</h2>
-
+    
         <div id="serviceWorkerList">
-          <h3> Service workers can make your site work offline, run faster, or both. Choose the functionality from our service workers below.</h3>
-
+          <h3> Service workers can make your site work offline, run faster, or both. Choose the functionality from our
+            service workers below.</h3>
+    
           <ul>
-            ${this.serviceWorkers.map((i) => html`<li class="serviceWorkerType"><input type="radio" @change=${() => this.updateChoice(i.id)} name="swgroup" value="${i.id}" id="selectsw${i.id}"><div class="swTypeInnerDiv"><h4>${i.title}</h4><span> ${i.description}</span></div></li> `)}
+            ${this.serviceWorkers.map((i) => html`<li class="serviceWorkerType"><input type="radio" @change=${()=>
+              this.updateChoice(i.id)} name="swgroup" value="${i.id}" id="selectsw${i.id}"><div class="swTypeInnerDiv">
+                <h4>${i.title}</h4><span> ${i.description}</span>
+              </div>
+            </li> `)}
           </ul>
         </div>
-
+    
         <div id="buttonDiv">
-          <button id="download" @click=${() => this.download()}>Add Service Worker</button>
-          <button id="preview" @click=${() => this.inspect()}> Preview</button>
+          <button id="download" @click=${()=> this.download()}>Add Service Worker</button>
+          <button id="preview" @click=${()=> this.inspect()}> Preview</button>
         </div>
       </div>
-
+    
       <div class="column">
         <h3>Add this code to your landing page in a &lt;script&gt; tag:</h3>
         <div>
           <pre>
-            <code>
-              ${this.webSiteData}
-            </code>
-          </pre>
+                <code>
+                
+                 ${unsafeHTML(this.highlightedWebSiteData)}
+                </code>
+              </pre>
         </div>
-
+    
         <h3>Add this code to your landing page in a &lt;script&gt; tag:</h3>
         <div>
           <pre>
-            <code>
-              ${this.serviceWorkerCode}
-            </code>
-          </pre>
+                <code>
+                  ${unsafeHTML(this.highlightedSWCode)}
+                </code>
+              </pre>
         </div>
       </div>
     </main>
